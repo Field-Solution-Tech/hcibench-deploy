a6f2a8c651d53c2dfde2404c9a196786aef352f6
Stephen Akuffo <12173427+sakuffo@users.noreply.github.com>
2025-06-24 05:21:30 -0700

    Update hcibench_deploy_with_val.ps1

---
+++ hcibench_deploy_with_val.ps1
@@ -0,0 +1,15 @@
+Connecting to vCenter: vc-wld01-a.site-a.vcf.lab
+Found datastore: cluster-wld01-01a-vsan01
+Looking for cluster: cluster-wld01-01a
+Using cluster deployment
+Looking for network 'hci-bench' on esx-07a.site-a.vcf.lab...
+Found network: hci-bench
+Deploying to: cluster-wld01-01a on host: esx-07a.site-a.vcf.lab
+Reading OVA configuration...
+Networks required by OVA:
+  - 
+Mapping networks to: hci-bench
+Configuring DHCP networking
+Deploying OVA...
+Write-Error: Deployment failed: 6/24/2025 5:19:10 AM	Import-VApp		Host did not have any virtual network defined.	
+PS /home/holuser/Desktop/code> 
